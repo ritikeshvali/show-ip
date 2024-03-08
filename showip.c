c49b80e135157b8cb1c2041436ffa99af94e777a
Ritikesh Vali <ritikeshvali2001@gmail.com>
2024-03-08 06:37:07 +0530

    Added comments to improve code readability

--- showip.c
+++ showip.c
@@ -8,51 +8,59 @@
 
 int main(int argc, char *argv[])
 {
+    // Structure to hold address information
     struct addrinfo hints, *res, *p;
     int status;
     char ipstr[INET6_ADDRSTRLEN];
 
+    // Check if hostname argument is provided
     if (argc != 2)
     {
         fprintf(stderr, "usage: showip hostname\n");
         return 1;
     }
 
+    // Clearing hints structure
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = AF_UNSPEC;     // Allow IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // Use TCP
 
+    // Retrieve address information corresponding to hostname
     if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0)
     {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
         return 2;
     }
 
+    // Display IP addresses for the provided hostname
     printf("IP addresses for %s:\n\n", argv[1]);
 
+    // Iterate through the linked list of address structures
     for (p = res; p != NULL; p = p->ai_next)
     {
         void *addr;
         char *ipver;
 
-        if (p->ai_family == AF_INET)
+        // Determine IP version and cast address accordingly
+        if (p->ai_family == AF_INET) // IPv4
         {
             struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
             addr = &(ipv4->sin_addr);
             ipver = "IPv4";
         }
-        else
+        else // IPv6
         {
             struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
             addr = &(ipv6->sin6_addr);
             ipver = "IPv6";
         }
 
+        // Convert binary IP address to presentation format
         inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
         printf(" %s: %s\n", ipver, ipstr);
     }
 
-    freeaddrinfo(res);
+    freeaddrinfo(res); // Free dynamically allocated memory
 
     return 0;
 }
